4168d94ebd456d8e1b4281f1a67a0d421ec09398
Jared Palmer <jared@MacBook-Pro.local>
2017-03-20 19:12:52 -0400

    Update template

--- packages/razzle/template/common/Home.js
+++ packages/razzle/template/common/Home.js
@@ -13,7 +13,7 @@ const Home = () => (
       <code>common/App.js and common/Home.js</code>
       {' '}and save to reload.
     </p>
-    <ul>
+    <ul style={{ marginBottom: '1rem' }}>
       <li><a href="https://github.com/jaredpalmer/razzle">Docs</a></li>
       <li><a href="https://github.com/jaredpalmer/razzle/issues">Issues</a></li>
       <li><a href="https://palmer.chat">Community Slack</a></li>
